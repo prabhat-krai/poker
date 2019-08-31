932823eaf9f91668950cad8b8eef145d87e21a2a
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-08-31 15:38:39 +0530

    adding test cases

--- game.py
+++ game.py
@@ -9,25 +9,32 @@ def hand_rank(hand):
     elif kind(4, ranks):                           # 4 of a kind
         return (7, kind(4, ranks), kind(1, ranks))
     elif kind(3, ranks) and kind(2, ranks):        # full house
-        return # your code here
+        return (6, kind(3, ranks), kind(2, ranks))
     elif flush(hand):                              # flush
-        return # your code here
+        return (5, ranks)
     elif straight(ranks):                          # straight
-        return # your code here
+        return (4, max(ranks))
     elif kind(3, ranks):                           # 3 of a kind
-        return # your code here
+        return (3, kind(3, ranks), ranks)
     elif two_pair(ranks):                          # 2 pair
-        return # your code here
+        return (2, kind(2, ranks), ranks)
     elif kind(2, ranks):                           # kind
-        return # your code here
+        return (1, kind(1, ranks), ranks)
     else:                                          # high card
-        return # your code here
+        return (0, ranks)
+
+def card_ranks():
+    pass
+
 
 def test():
     "Test cases for the functions in poker program"
     sf = "6C 7C 8C 9C TC".split() # Straight Flush
     fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
     fh = "TD TC TH 7C 7D".split() # Full House
+    assert card_ranks(sf) == [10, 9, 8, 7, 6]
+    assert card_ranks(fk) == [9, 9, 9, 9, 7]
+    assert card_ranks(fh) == [10, 10, 10, 7, 7]
     assert poker([sf, fk, fh]) == sf
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
