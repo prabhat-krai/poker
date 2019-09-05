14086c78150c7c9e2d78e6fcebdd46a8835bf2df
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-09-05 19:02:00 +0530

    Learnt to use generator expression and heuristics to stop exploration of search space

--- zebra_puzzle.py
+++ zebra_puzzle.py
@@ -1,3 +1,41 @@
 # Solving the interesting puzzle. You can check it out using
 # https://en.wikipedia.org/wiki/Zebra_Puzzle Use this to 
 # read about it
+
+import itertools
+
+def imright(h1, h2):
+    "House h1 is immediately right of h2 if h1-h2 == 1."
+    return h1-h2 == 1
+
+def nextto(h1, h2):
+    "Two houses are next to each other if they differ by 1."
+    return abs(h1-h2) == 1
+
+def zebra_puzzle():
+    "Return a tuple (WATER, ZEBRA indicating their house numbers."
+    houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
+    orderings = list(itertools.permutations(houses)) # 1
+    return next((water, zebra)
+                for (red, green, ivory, yellow, blue) in orderings
+                if imright(green, ivory)
+                for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in orderings
+                if Englishman is red
+                if Norwegian is first
+                if nextto(Norwegian, blue)
+                for (coffee, tea, milk, oj, water) in orderings
+                if coffee is green
+                if Ukranian is tea
+                if milk is middle
+                for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+                if Kools is yellow
+                if LuckyStrike is oj
+                if Japanese is Parliaments
+                for (dog, snails, fox, horse, zebra) in orderings
+                if Spaniard is dog
+                if OldGold is snails
+                if nextto(Chesterfields, fox)
+                if nextto(Kools, horse)
+                )
+
+print(zebra_puzzle())
