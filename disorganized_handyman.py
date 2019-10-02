9390a02a607e97072db87161f6f3d14d198fdb28
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-03 02:32:05 +0530

    solved

--- disorganized_handyman.py
+++ disorganized_handyman.py
@@ -19,4 +19,44 @@ O(n^2) solution.
 So, we can should look to sort the lists and then each nut will be matched to its bolt.
 We will use quicksort and that too inplace so that we can work with small memory.
 """
+def pivot_partition_inplace(lst, start, end):
+    pivot = lst[end] 
+    bottom = start - 1       
+    top = end
 
+    done = False
+    while not done: 
+
+        while not done:
+            bottom += 1 
+            if (bottom == top): 
+                done = True 
+                break
+            if (lst[bottom] > pivot): 
+                lst[top] = lst[bottom] 
+                break 
+
+        while not done:
+            top -= 1
+            if (top == bottom): 
+                done = True 
+                break
+            if (lst[top] < pivot): 
+                lst[bottom] = lst[top] 
+                break 
+
+    lst[top] = pivot 
+    return top 
+
+
+def quicksort(lst, start, end):
+    if start < end: 
+        split = pivot_partition_inplace(lst, start, end) 
+        quicksort(lst, start, split - 1)
+        quicksort(lst, split + 1, end)
+    return
+    
+quicksort(nuts, 0, n - 1)
+quicksort(bolts, 0, n - 1)
+
+print(nuts, bolts)
