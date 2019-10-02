2a0014c1ce7ae99e9b2f1bfa2be6db49edcbcdd4
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-02 23:57:15 +0530

    Problem posed

---
+++ disorganized_handyman.py
@@ -0,0 +1,5 @@
+"""
+A disorganized handyman has mixed up his nuts and bolts in a bag and your task is to match them as 
+quickly as possible. Curiously, as described in this video, 
+a famous algorithm for sorting numbers, quicksort, provides an insight into solving this problem.
+"""
