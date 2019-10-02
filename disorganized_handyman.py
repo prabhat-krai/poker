413665b87856b5d12b88fa5d73daae9ffbbeb4f0
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-03 00:22:49 +0530

    made lists for nuts and bolts

--- disorganized_handyman.py
+++ disorganized_handyman.py
@@ -1,5 +1,15 @@
 """
 A disorganized handyman has mixed up his nuts and bolts in a bag and your task is to match them as 
-quickly as possible. Curiously, as described in this video, 
-a famous algorithm for sorting numbers, quicksort, provides an insight into solving this problem.
+quickly as possible. 
 """
+import random
+
+n = int(input("Number of nut and bolts in total"))
+nuts = [0]*n
+bolts = [0]*n
+types = int(input("The different categories of nuts and bolts available:"))
+for i in range (n):
+    number_generated = random.randint(1, types)
+    nuts[i] = bolts[i] = number_generated
+random.shuffle(bolts)
+
