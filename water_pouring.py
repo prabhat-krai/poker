a8462555ddf174d89a31b9627a909dff1ee9bfb3
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-03 15:51:38 +0530

    Solved. Need to be revisited.

--- water_pouring.py
+++ water_pouring.py
@@ -4,6 +4,16 @@ We need to calculate 6 litres using these two.
 We also have an unlimited source of water.
 """
 
+def successors(x, y, X, Y):
+    if (x <= X and y <= Y): #checking that the water levels are less than the glass can take
+        return {
+            ((0, y+x) if y+x<Y else (x - (Y-y), y + (Y-y))): 'X -> Y',
+            ((y+x, 0) if y+x<X else (x + (X-x), y - (X-x))): 'X <- Y',
+            (X, y): 'fill X', (x, Y): 'fill Y',
+            (0, y): 'empty X', (x, 0): 'empty Y'
+            }
+
+
 def pour_problem(X, Y, goal, start = (0,0)):
     if goal in start:
         return [start] #converting into list while returning
@@ -24,3 +34,4 @@ def pour_problem(X, Y, goal, start = (0,0)):
 
     return "Failed to find a way to reach the goal"
 
+print(pour_problem(9,4,2))
