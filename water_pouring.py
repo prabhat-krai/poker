88d6820a311c24b4c7ed3d2d5e8771537cc83b3d
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-03 15:39:00 +0530

    Finding a way to find successors

--- water_pouring.py
+++ water_pouring.py
@@ -3,3 +3,24 @@ We have 2 flasks of capacity 9 litres and 4 liters.
 We need to calculate 6 litres using these two. 
 We also have an unlimited source of water.
 """
+
+def pour_problem(X, Y, goal, start = (0,0)):
+    if goal in start:
+        return [start] #converting into list while returning
+
+    explored = set()
+    frontier = [[ start ]]
+    while frontier:
+        path = frontier.pop(0) #first path
+        (x, y) = path[-1]     #last state
+        for (state, action) in successors(x, y, X,Y).items():
+            if state not in explored:
+                explored.add(state)
+                path_new = path + [action, state]
+                if goal in state:
+                    return path_new
+                else:
+                    frontier.append(path_new)
+
+    return "Failed to find a way to reach the goal"
+
