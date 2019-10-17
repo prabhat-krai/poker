b4a3b9780f2aba2a534a83517996f16a497c5580
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-17 18:47:32 +0530

    added different matching schemes

--- regularEx.py
+++ regularEx.py
@@ -1,1 +1,121 @@
 # Using RegEx as a tool to understand languages.
+
+# * gives us the ability to match like baa, baaa, baaa with ba*
+# ? has the same ability as * but for single char
+# . returns single characters
+# ^ checks for starts with
+# $ checks for ends with 
+
+def match(pattern, text):
+    if pattern == '':
+        return True
+    elif pattern == '$':
+        return (text == '')
+    elif len(pattern) > 1 and pattern[1] in '*?':
+        p, op, pat = pattern[0], pattern[1], pattern[2:]
+        if op == '*':
+            return match_star(p, pat, text)
+        elif op == '?':
+            if match1(p, text) and match(pat, text[1:]):
+                return True
+            else:
+                return match(pat, text)
+    else:
+        return (match1(pattern[0], text) and
+                match( pattern[1:],text[1:])) # fill in this line
+
+def match1(p, text):
+    if not text: return False
+    return p == '.' or p == text[0]
+
+def match_star(p, pattern, text):
+    return (match(pattern, text) or 
+            match1(p, text) and 
+            match_star(p, pattern, text[1:])))
+
+
+
+#Grammar of a language is on which regEx works
+#UI is used by the user while APIs are used by programmers
+#Writing different language processing functions now
+
+
+
+def matchset(pattern, text):
+    "Match pattern at start of text; return a set of remainders of text."
+    op, x, y = components(pattern)
+    if 'lit' == op:
+        return set([text[len(x):]]) if text.startswith(x) else null
+    elif 'seq' == op:
+        return set(t2 for t1 in matchset(x, text) for t2 in matchset(y, t1))
+    elif 'alt' == op:
+        return matchset(x, text) | matchset(y, text)
+    elif 'dot' == op:
+        return set([text[1:]]) if text else null
+    elif 'oneof' == op:
+        return set([text[1:]]) if text.startswith(x) else null
+    elif 'eol' == op:
+        return set(['']) if text == '' else null
+    elif 'star' == op:
+        return (set([text]) |
+                set(t2 for t1 in matchset(x, text)
+                    for t2 in matchset(pattern, t1) if t1 != text))
+    else:
+        raise ValueError('unknown pattern: %s' % pattern)
+        
+null = frozenset()
+
+def components(pattern):
+    "Return the op, x, and y arguments; x and y are None if missing."
+    x = pattern[1] if len(pattern) > 1 else None
+    y = pattern[2] if len(pattern) > 2 else None
+    return pattern[0], x, y
+   
+def test():
+    assert matchset(('lit', 'abc'), 'abcdef')            == set(['def'])
+    assert matchset(('seq', ('lit', 'hi '),
+                     ('lit', 'there ')), 
+                   'hi there nice to meet you')          == set(['nice to meet you'])
+    assert matchset(('alt', ('lit', 'dog'), 
+                    ('lit', 'cat')), 'dog and cat')      == set([' and cat'])
+    assert matchset(('dot',), 'am i missing something?') == set(['m i missing something?'])
+    assert matchset(('oneof', 'a'), 'aabc123')           == set(['abc123'])
+    assert matchset(('eol',),'')                         == set([''])
+    assert matchset(('eol',),'not end of line')          == frozenset([])
+    assert matchset(('star', ('lit', 'hey')), 'heyhey!') == set(['!', 'heyhey!', 'hey!'])
+    
+    return 'tests pass'
+
+print test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
